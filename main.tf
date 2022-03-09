2b8840cd5ff1dd833e660a9ce90e4c0997dfca8c
Satish <satishkumar.m1@yahoo.com>
2022-03-08 17:23:50 -0800

    Updated

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
 data "aws_vpc" "main" {
   id = var.vpc_id
- # id = "vpc-e1fd5787"
-
-}
+ }
 
 
 resource "aws_security_group" "sg_my_server" {
@@ -51,12 +49,10 @@ resource "aws_security_group" "sg_my_server" {
 }
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
-  #public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDIYs7KN2jF9nEKTvWHquoV4qqTgtHfGRomAGhSL1KXYHnPWfAZGk92yqp6yUCYh+kLhVU91EaAmwljSK89MkyRS8djsmSGDXmEaDNOypTUkdpO33rtvzx5Q40M/nfIzCAJA2xJxWk7IFKAGhGc5cUNPI+t5jCsYsdV7pPgVOY5QXGDSraZpTV5r56Pe2lQMBhknZGVw4gCQZhFOHdqzVLGwB4NLno8sfyr5F62F6kg5jTRTWSSIoEywQFD1uBzoBPe58SCZByJIPSFW3kOhNjix/FoeBccPvGYMPeZep89PpwKNUWCGAnXEbYIHcBshg9j4TmtmXjtJnV5bTL3jWoIfvGzn4qxOsLVPBEny88SkHIkN44Q2QoT3sF8TWogoULKS6JoW41dp4nUXYA5nsZinI5pp1e5oaocg8dnl4ewlfeWxw+GK2GX4naWnykmizkiSIhNYS8lFF2iDMRTJ6W/JTOp19ginhYL/7D3sMeDKNIIntt8BVdc4je+jbX37Bs= user@LAPTOP-QCJ1EQDV"
   public_key = var.public_key
 }
 
 data "template_file" "user_data" {
- # template = file("./terraform-aws-apache-example/userdata.yaml")
  template = file("${abspath(path.module)}/userdata.yaml")
 }
 
@@ -76,7 +72,6 @@ data "aws_ami" "amazon-linux-2" {
 }
 
 resource "aws_instance" "my_server" {
-  #ami           = "ami-051317f1184dd6e92"
   ami = "${data.aws_ami.amazon-linux-2.id}"
   instance_type = var.instance_type
   key_name = "${aws_key_pair.deployer.key_name}"
